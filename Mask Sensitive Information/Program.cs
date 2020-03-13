2b776b47f25b4bb459857f04e48ccf381bc98d01
Ivan Phelps <ivanniothemannio@gmail.com>
2020-03-13 15:52:48 -0500

    Add project files.

---
+++ Mask Sensitive Information/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Mask_Sensitive_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your credit card number?");
+            string secret = Console.ReadLine();
+            string lastFour = secret.Substring(secret.Length - 4);
+            string firstPart = secret.Substring(0, secret.Length - 4);
+
+            foreach (char character in firstPart)
+            {
+                firstPart = firstPart.Replace(character, 'X');
+            }
+
+            string result = firstPart + lastFour;
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+
+        }
+    }
+}
